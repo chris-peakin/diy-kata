c4d364ddf3bf26ad6f3a788d5a6e19adc63ba49b
Chris Peakin <christopher.peakin@googlemail.com>
2021-12-04 15:00:21 +0000

    completed 5th kata

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,6 @@
-const reachDestination = (distance, speed) => {};
-
+const reachDestination = (distance, speed) => {
+    let distanceOverTime = distance / speed;
+    let estimateTime = Math.round(distanceOverTime*2)/2;
+    return `I should be there in ${estimateTime} hours.`
+};
 module.exports = reachDestination;
