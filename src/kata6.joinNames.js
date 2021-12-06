9fe4b12e70c547c1a82704b3d3222bda34dab0cc
Chris Peakin <christopher.peakin@googlemail.com>
2021-12-06 21:36:37 +0000

    added comment to kata 6

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -2,6 +2,11 @@ const joinNames = namesObj => {
     let newString ='';
     for (let i = 0; i < namesObj.length; i += 1){
         if (i < (namesObj.length - 1)){
+            /*Note: {namesObj[i].name} requires [i] before the dot,
+            otherwise it's returning undefined. There's confusion between
+            dot and bracket notation here. By having [i] before the object's
+            property, at least I'm finding the object first (by its index),
+            then the name key.*/ 
             newString += `${namesObj[i].name}, `
         } else {
             newString += `& ${namesObj[i].name}`
