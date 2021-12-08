582c57823649ae0fcdb456a93c084b9ea06c59ee
Chris Peakin <christopher.peakin@googlemail.com>
2021-12-08 07:00:27 +0000

    updated kata 3

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,8 +1,18 @@
 const numberToReversedDigits = number => {
+    /*This turns the array into a string, still in the same order
+    as before. The split('') means there's nothing separating the digits; 
+    in other situations, we'd separate with a comma or dash etc.*/
     let initialArray = (""+number).split('');
     let reversedArray = [];
+    /*For loop cycles through the array, starting at the very end.
+    It requires i to start at the array's length - 1 (as arrays are zero-indexed)
+    and decreases i by 1 until i is 0, 0 being the index of
+    the initial array's first element.*/
     for (let i = initialArray.length - 1; i >= 0; i--){
-        reversedArray[i] = parseInt(initialArray.shift(), 10);
+    /*We're still dealing with a string, so parseInt turns the string's index
+    into an integer, then adds it to the new array. We're starting
+    from the very end of initialArray.*/
+        reversedArray[i] = parseInt(initialArray.shift());
     }
     return reversedArray;
 };
--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -3,5 +3,7 @@ const { numberToReversedDigits } = require("../src");
 describe("numberToReversedDigits", () => {
   test("returns a reversed array of the number's digits", () => {
     expect(numberToReversedDigits(12345)).toEqual([5,4,3,2,1])
+    expect(numberToReversedDigits(54321)).toEqual([1,2,3,4,5])
+    expect(numberToReversedDigits(12)).toEqual([2,1])
   });
 });
