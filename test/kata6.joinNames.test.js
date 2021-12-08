0a84bc498d7da24125b5a724705473764357c843
Chris Peakin <christopher.peakin@googlemail.com>
2021-12-08 06:49:26 +0000

    updated kata 6

--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -2,6 +2,12 @@ const { joinNames } = require("../src");
 
 describe("joinNames", () => {
   test("returns string of names, seperated by commas and an ampersand", () => {
+    /*The tests will be run from top to bottom. It's important
+    to increase the array's length as we go from top to bottom
+    so that during testing, situations are evaluated one at a time
+    and improve the function from there. The array's length being 1, then
+    2, then more than 2 are the major situations addressed.*/
+    expect(joinNames([])).toBe('')
     expect(joinNames([{name: 'Han'}])).toBe('Han')
     expect(joinNames([{name: 'Jack'}, {name: 'Jill'}])).toBe('Jack & Jill')
     expect(joinNames([{name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'}])).toBe('Bart, Lisa & Maggie')
