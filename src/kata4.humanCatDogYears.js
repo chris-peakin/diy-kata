e4032194f17bb41559615af0c94563bf3ab9d3a9
Chris Peakin <christopher.peakin@googlemail.com>
2021-12-09 20:10:55 +0000

    updated kata 4 with substantial improvements

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,14 +1,24 @@
 const humanCatDogYears = number => {
-    let array = [number];
     let catYears = 0;
     let dogYears = 0;
-    if (number > 1){
+    if (number === 0){
+        return [number, catYears, dogYears]
+    } else if (number === 1){
+        catYears += 15;
+        dogYears += 15;
+        return [number, catYears, dogYears]
+    } else if(number === 2){
+        //15 + 9 is 24
+        catYears += 24;
+        dogYears += 24;
+        return [number, catYears, dogYears]
+    } else if (number > 2){
         catYears += (15 + 9 + (4*(number-2)));
         dogYears += (15 + 9 + (5*(number-2)));
-    }
-    array.push(catYears);
-    array.push(dogYears);
-    return array
+        return [number, catYears, dogYears]
+    } else if (number < 0){
+        return 'error!';
+    };
 };
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -2,7 +2,30 @@ const { humanCatDogYears } = require("../src");
 
 
 describe("humanCatDogYears", () => {
-    it("returns [10, 56, 64] when 10 is passed", () =>{
+    it("returns [0, 0, 0] if 0 is passed, incase the user enters 0, however unlikely", () => {
+        expect(humanCatDogYears(0)).toEqual([0, 0, 0]);
+    })
+
+    it("returns [1, 15, 15] if 1 is passed", () =>{
+        expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
+    })
+
+    it ("returns [2, 24, 24] if 2 is passed", () => {
+        expect(humanCatDogYears(2)).toEqual([2, 24, 24]);
+    })
+
+    it ("returns [3, 28, 29] if 3 is passed, the lowest number where cats and dogs diverge in age", () => {
+        expect(humanCatDogYears(3)).toEqual([3, 28, 29]);
+    })
+
+    it("returns [10, 56, 64] when 10 is passed for example", () =>{
         expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+        expect(humanCatDogYears(15)).toEqual([15, 76, 89]);
+    })
+
+    it("returns an error if a negative number is passed", () => {
+        expect(humanCatDogYears(-1)).toEqual('error!');
+        expect(humanCatDogYears(-2)).toEqual('error!');
+        expect(humanCatDogYears(-15)).toEqual('error!');
     })
 }) 
